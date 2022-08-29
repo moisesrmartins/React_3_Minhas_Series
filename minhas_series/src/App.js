dc884b9c0b8ebda56292f8800901c90719694b3c
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-08-29 15:13:25 -0300

    React_3_Minhas_Series Parte 4: Navbar e NavbarBrand

--- minhas_series/src/App.js
+++ minhas_series/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {
+  Navbar,
+  NavbarBrand
+} from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-    <h1>Minhas Séries</h1>
+    <Navbar color = "light" light expand = "md">
+      <NavbarBrand>Minhas Séries</NavbarBrand>
+    </Navbar>
     </div>
   );
 }
