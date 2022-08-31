5afa46c13d18d585c0fb0bf631418d38a96aea93
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-08-31 16:32:30 -0300

    React_3_Minhas_Series Parte 10: Axios e Servidor

--- minhas_series/src/App.js
+++ minhas_series/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Home = () => {
-  return <h1>Home</h1>
-}
+  return <h1>Home</h1>;
+};
 const Genres = () => {
-  return <h1>Genres</h1>
-}
+  return <h1>Genres</h1>;
+};
 
-function App () {
+function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    axios.get("/api").then((ans) => {
+      setData(ans.data);
+    });
+  }, []);
+
   return (
     <div>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path = "/" element = {<Home/>}/>
-        <Route path = "/Genres" element = {<Genres/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Genres" element={<Genres />} />
       </Routes>
+      <pre>{JSON.stringify(data)}</pre>
     </div>
   );
 }
