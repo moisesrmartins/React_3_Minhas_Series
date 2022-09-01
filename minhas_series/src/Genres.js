86919f2841d86f9da7fd5082e66c9e4748f10ff8
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-01 10:09:46 -0300

    React_3_Minhas_Series Parte 12: Table

--- minhas_series/src/Genres.js
+++ minhas_series/src/Genres.js
@@ -4,14 +4,30 @@ import axios from "axios";
 const Genres = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get("/genres").then((ans) => {
-      setData(ans.data);
+    axios.get("/api/genres").then((ans) => {
+      setData(ans.data.data);
     });
   }, []);
 
   return (
     <div>
       <h1>Genres</h1>
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Moisés</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
       <pre>{JSON.stringify(data)}</pre>
     </div>
   );
