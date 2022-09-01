f6208079228aae62135b87ca670a75b01cf49638
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-01 16:02:14 -0300

    React_3_Minhas_Series Parte 15: Event

--- minhas_series/src/NewGenre.js
+++ minhas_series/src/NewGenre.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react";
 
 const NewGenre = () => {
+  const [name, setName] = useState("");
+  const onChange = (event) => {
+    setName(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
     <div className="container">
       <h1>New Genre</h1>
       <form>
-        <div class="form-group">
-          <label for="name">Name</label>
+        <div className="form-group">
+          <label htmlfor="name">Name</label>
           <input
             type="text"
+            value={name}
+            onChange={onChange}
             className="form-control"
             id="name"
             placeholder="Genre Name"
           />
-          <button type="button" class="btn btn-primary">
+          <button type="button" className="btn btn-primary">
             Save Gender
           </button>
         </div>
