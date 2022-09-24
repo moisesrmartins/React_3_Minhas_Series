8b9a05e253513a4c63e98954a505ebdb8fb9ffed
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-24 11:08:20 -0300

    React_3_Minhas_Series Parte 30: Genre e form-check

--- my_series/src/InfoSeries.js
+++ my_series/src/InfoSeries.js
@@ -4,19 +4,39 @@ import { Navigate, useParams } from "react-router-dom";
 import { Badge } from "reactstrap";
 
 const InfoSeries = ({ match }) => {
-  const [form, setForm] = useState({});
+  const [form, setForm] = useState({
+    name: "",
+  });
   const [success, setSuccess] = useState(false);
   const [mode, setMode] = useState("Edit/Coment");
+  const [genres, setGenres] = useState([]);
+  const [genreId, setGenreId] = useState("");
+  const [genreName, setGenreName] = useState("");
 
   let { id } = useParams(match);
 
   const [data, setData] = useState({});
   useEffect(() => {
+    console.log(id);
     axios.get("/api/Series/" + id).then((ans) => {
       setData(ans.data);
       setForm(ans.data);
     });
   }, [id]);
+
+  useEffect(() => {
+    axios.get("/api/Genres").then((res) => {
+      setGenres(res.data.data);
+      const genres = res.data.data;
+      console.log("genreId", id);
+      const finder = genres.find((value) => data.genre !== value.name);
+      if (finder) {
+        setGenreId(finder.id);
+        setGenreName(finder.name);
+      }
+      console.log("find", finder);
+    });
+  }, [data.genre, data.name, id]);
 
   const masterHeader = {
     height: "50vh",
@@ -27,16 +47,28 @@ const InfoSeries = ({ match }) => {
     backgroundRepeat: "no-repeat",
   };
 
+  const onChangeGenre = (genreId) => {
+    setGenreId(genreId.target.value);
+    console.log(genreId.target.value);
+  };
+
   const onChange = (field) => (event) => {
     setForm({ ...form, [field]: event.target.value });
     console.log(event.target.value);
   };
 
+  const select = (value) => () => {
+    setForm({ ...form, status: value });
+  };
+
   const save = () => {
-    axios.put("/api/Series" + id, form).then((res) => {
-      setSuccess(true);
-      console.log(res);
-    });
+    console.log(form, genreId);
+    axios
+      .put("/api/Series/" + id, { ...form, genre_id: genreId })
+      .then((res) => {
+        setSuccess(true);
+        console.log(res);
+      });
   };
 
   if (success) {
@@ -60,10 +92,17 @@ const InfoSeries = ({ match }) => {
               <div className="col-8">
                 <h1 className="font-weight-light text-white">{data.name}</h1>
                 <div className="lead text-white">
-                  <Badge color="success">Watched</Badge>
+                  {data.status === "To Watch" && (
+                    <Badge color="warning">To Watch</Badge>
+                  )}
                 </div>
                 <div className="lead">
-                  <Badge color="warning">To Watch</Badge>
+                  {data.status === "Watched" && (
+                    <Badge color="success">Watched</Badge>
+                  )}
+                </div>
+                <div className="lead text-white" onChange={onChangeGenre}>
+                  Genre: {genreName}
                 </div>
               </div>
             </div>
@@ -71,7 +110,7 @@ const InfoSeries = ({ match }) => {
         </div>
       </header>
 
-      <div>
+      <div className="container">
         <button
           type="button"
           className="btn btn-primary"
@@ -83,8 +122,9 @@ const InfoSeries = ({ match }) => {
 
       {mode === "Edit/Coment" && (
         <div className="container">
-          <h1>Info Series</h1>
+          <h1>Edit/Coment</h1>
           <pre>{JSON.stringify(form)}</pre>
+
           <form>
             <div className="form-group">
               <label htmlFor="name">Name</label>
@@ -107,9 +147,57 @@ const InfoSeries = ({ match }) => {
                 placeholder="Coment Series"
               />
 
+              <div className="form-group">
+                <label htmlFor="name">Genres</label>
+                <select
+                  className="form-control"
+                  onChange={onChangeGenre}
+                  value={genreId}
+                >
+                  {genres.map((genre) => (
+                    <option key={genre.id} value={genres.id}>
+                      {genre.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="status"
+                  id="To Watch"
+                  value="To Watch"
+                  onChange={select("To Watch")}
+                  checked={form.status === "To Watch"}
+                />
+
+                <label className="form-check-label" htmlFor="To Watch">
+                  To Watch
+                </label>
+              </div>
+
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="status"
+                  id="Watched"
+                  value="Watched"
+                  onChange={select("Watched")}
+                  checked={form.status === "Watched"}
+                />
+
+                <label className="form-check-label" htmlFor="Watched">
+                  Watched
+                </label>
+              </div>
+
               <button type="button" onClick={save} className="btn btn-success">
                 Save Edit/Coment
               </button>
+
               <button
                 type="button"
                 className="btn btn-danger"
